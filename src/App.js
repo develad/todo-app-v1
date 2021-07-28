decccff0c7c9d44a1cb3d89a068d55543ba747a9
develad <develadb@gmail.com>
2021-07-28 17:33:16 +0300

    :tada: final version!

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import Home from './components/Home';
 
 function App() {
   return (
-    <>
+    <div className='app-cont-new'>
       <h1
-        style={{ marginTop: '50px', fontSize: '3.5rem', textAlign: 'center' }}
+        style={{
+          marginTop: '50px',
+          fontSize: '3.5rem',
+          textAlign: 'center',
+          padding: '10px 20px',
+          border: '5px solid white',
+          borderRadius: '15px',
+          backgroundColor: 'black',
+        }}
       >
         To-do app
       </h1>
       <Home />
-    </>
+    </div>
   );
 }
 
