7eba2b3cf29776c49d9b7decea713fd89837cce6
develad <develadb@gmail.com>
2021-07-30 17:15:06 +0300

    :tada: objToCss

--- fromObjToCssStyle.js
+++ fromObjToCssStyle.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const arr = {
+const arrObj = {
   position: 'fixed',
   bottom: '10px',
   right: '10px',
@@ -11,22 +11,42 @@ const arr = {
   border: '5px solid white',
   outline: 'none',
   borderRadius: '50%',
+  textShadow: '3px 3px 5px black',
+  textDecoration: 'none',
+  color: 'white',
+  fontSize: '20px',
 };
 
-const brr = [];
-for (const item in arr) {
-  const cssStr = `${item}: ${arr[item]};`;
-  brr.push(cssStr);
-}
+// converting zIndex into z-index
+const changeString = (word) => {
+  const letterArray = word.split('');
+  for (let i = 0; i < letterArray.length; i++) {
+    let character = letterArray[i];
+    if (character === character.toUpperCase()) {
+      letterArray[i] = `-${character.toLowerCase()}`;
+    }
+  }
+  return letterArray.join('');
+};
 
-let myStr = '';
-brr.map((item) => {
-  let replacer = item.split(':')[0];
-  if (replacer === 'zIndex') replacer = 'z-index';
-  if (replacer === 'borderRadius') replacer = 'border-radius';
-  myStr += `${replacer}:${item.split(':')[1]}\n`;
-  return '😉';
-});
+const intoCss = (arr) => {
+  const brr = [];
+  for (const item in arr) {
+    const cssStr = `${item}: ${arr[item]};`;
+    brr.push(cssStr);
+  }
 
-console.log(myStr);
-fs.writeFileSync('myCss.txt', myStr);
+  let myStr = '';
+  brr.map((item) => {
+    // let replacer = item.split(':')[0];
+    // if (replacer === 'zIndex') replacer = 'z-index';
+    // if (replacer === 'borderRadius') replacer = 'border-radius';
+    let replacer = changeString(item.split(':')[0]);
+    myStr += `${replacer}:${item.split(':')[1]}\n`;
+    return '😉';
+  });
+  return myStr;
+};
+
+console.log(intoCss(arrObj));
+fs.writeFileSync('myCss.txt', intoCss(arrObj));
